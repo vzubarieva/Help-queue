8ac3e8f7a388673f81a992d41388d7317fd7da35
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-10-11 13:58:51 -0700

    show image in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ticketsImage from "./../img/tickets.png";
 
 function Header() {
-  return <h1>Help Queue</h1>;
+  return (
+    <React.Fragment>
+      <img src={ticketsImage} alt="tickets" />
+    </React.Fragment>
+  );
 }
 
 export default Header;
--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Ticket(props) {
   return (
@@ -13,5 +14,11 @@ function Ticket(props) {
     </React.Fragment>
   );
 }
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string,
+};
 
 export default Ticket;
