bfebc174d711844b889041cea9049618cf2bba48
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-10-10 20:26:36 -0700

    add data between componenets

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,16 +1,13 @@
 import React from "react";
 
-function Ticket() {
-  const name = "Thato";
-  const name2 = "Haley";
+function Ticket(props) {
   return (
     <React.Fragment>
-      <h3>3a</h3>
       <h3>
-        {name} and {name2}
+        {props.location} - {props.names}
       </h3>
       <p>
-        <em>Firebase entries not saving!</em>
+        <em>{props.issue}</em>
       </p>
       <hr />
     </React.Fragment>
