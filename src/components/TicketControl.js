5f570b680da0a215b7a1f63ff4d7fcb265866cb9
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-10-18 20:07:06 -0700

    add ticket detail functionality

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,13 +1,15 @@
 import React from "react";
 import NewTicketForm from "./NewTicketForm";
 import TicketList from "./TicketList";
+import TicketDetail from "./TicketDetail";
 
 class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false,
-      mainTicketList: [], // new code
+      formVisibleOnPage: false, // tate slice determines whether or not a form should show on the page. It is local state.
+      mainTicketList: [], //  second state slice holds the list of all tickets. It is shared state.
+      selectedTicket: null, //third state slice will determine whether our TicketDetail component should show or not
     };
     this.handleClick = this.handleClick.bind(this); //new code here
   }
@@ -20,25 +22,51 @@ class TicketControl extends React.Component {
     });
   };
 
+  handleChangingSelectedTicket = (id) => {
+    const selectedTicket = this.state.mainTicketList.filter(
+      (ticket) => ticket.id === id
+    )[0];
+    this.setState({ selectedTicket: selectedTicket });
+  };
+
   handleClick = () => {
-    this.setState((prevState) => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage,
-    }));
+    if (this.state.selectedTicket != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTicket: null,
+      });
+    } else {
+      this.setState((prevState) => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
   };
 
   render() {
     let currentlyVisibleState = null;
     let buttonText = null; // new code
-    if (this.state.formVisibleOnPage) {
+
+    if (this.state.selectedTicket != null) {
+      currentlyVisibleState = (
+        <TicketDetail ticket={this.state.selectedTicket} />
+      );
+
+      buttonText = "Return to Ticket List";
+      // While our TicketDetail component only takes placeholder data, we will eventually be passing the value of selectedTicket as a prop.
+    } else if (this.state.formVisibleOnPage) {
       currentlyVisibleState = (
         <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
       );
       buttonText = "Return to Ticket List"; // new code
     } else {
       currentlyVisibleState = (
-        <TicketList ticketList={this.state.mainTicketList} />
+        <TicketList
+          ticketList={this.state.mainTicketList}
+          onTicketSelection={this.handleChangingSelectedTicket}
+        />
       );
-      buttonText = "Add Ticket"; // new code
+      // Because a user will actually be clicking on the ticket in the Ticket component, we will need to pass our new handleChangingSelectedTicket method as a prop.
+      buttonText = "Add Ticket";
     }
     return (
       <React.Fragment>
--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -25,19 +25,16 @@ function TicketList(props) {
   return (
     <React.Fragment>
       <hr />
-      {props.ticketList.map(
-        (
-          ticket,
-          index // Loop through the list passed down from TicketControl.
-        ) => (
-          <Ticket
-            names={ticket.names}
-            location={ticket.location}
-            issue={ticket.issue}
-            key={index}
-          />
-        )
-      )}
+      {props.ticketList.map((ticket) => (
+        <Ticket
+          whenTicketClicked={props.onTicketSelection}
+          names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          id={ticket.id}
+          key={ticket.id}
+        />
+      ))}
     </React.Fragment>
   );
 }
@@ -45,6 +42,7 @@ function TicketList(props) {
 // Add propTypes for ticketList.
 TicketList.propTypes = {
   ticketList: PropTypes.array,
+  onTicketSelection: PropTypes.func,
 };
 
 export default TicketList;
